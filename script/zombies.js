c7dc823e78ba27ee7315da1268d5e7ddc21727cd
Kenneth Jansson <kenneth.jansson@gmail.com>
2024-04-19 22:26:07 +0200

    started fixing  movePlayer()
    Only dissapears but at least its found and altered. Change approach and work with updating affected divs from corresponding matrixItems after changing poisitions.

--- script/zombies.js
+++ script/zombies.js
@@ -1,9 +1,10 @@
 let matrixContainer = document.querySelector("game-window")
 const rows = 5
 const columns = 5
-let gameMatrix = []
+const gameMatrix = []
 const catPositions = []
 const matrixItems = []
+const matrixBackgroundElements = []
 
 class Player {
     constructor() {
@@ -32,35 +33,6 @@ class Forest {
     }
 }
 
-function checkResult(newPosition) {
-    for (let i = 0; i < zombiePositions.length - 1; i++)
-        if (newposition[0] == zombiePositions[i][0] && newposition[1] == zombiePositions[i][1]) {
-            return 'You died!!'
-        }
-}
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-function findPlayerIndexes() {
-    let playerIndexes
-    for (let i = 0; i < gameMatrix.length; i++) {
-        for (let j = 0; j < gameMatrix[i].length; j++) {
-            if (gameMatrix[i][j] instanceof Player) {
-                playerIndexes = [i, j]
-                break
-            }
-        }
-        if (playerIndexes) break
-    }
-    return playerIndexes
-}
-function moveZombies() {
-
-}
 
 function fillMatrixItems() {
     let unshuffledItems = []
@@ -87,67 +59,6 @@ function fillMatrixItems() {
             }
         }
         matrixItems.push(row)
-
-    }
-}
-
-function movePlayer(direction) {
-    let playerIndexes = findPlayerIndexes()
-    let tempPlayer = gameMatrix[playerIndexes[0]][playerIndexes[1]]
-
-    switch (direction) {
-        case 'N':
-            if (playerIndexes[0] > 0) {
-                gameMatrix[playerIndexes[0]][playerIndexes[1]] = new Forest()
-                gameMatrix[playerIndexes[0] - 1][playerIndexes[1]] = tempPlayer
-            }
-            break;
-        case 'E':
-            if (playerIndexes[1] < gameMatrix[0].length - 1) {
-                gameMatrix[playerIndexes[0]][playerIndexes[1]] = new Forest()
-                gameMatrix[playerIndexes[0]][playerIndexes[1] + 1] = tempPlayer
-            }
-            break;
-        case 'S':
-            if (playerIndexes[0] < gameMatrix.length - 1) {
-                gameMatrix[playerIndexes[0]][playerIndexes[1]] = new Forest()
-                gameMatrix[playerIndexes[0] + 1][playerIndexes[1]] = tempPlayer
-            }
-            break;
-        case 'W':
-            if (playerIndexes[1] > 0) {
-                gameMatrix[playerIndexes[0]][playerIndexes[1]] = new Forest()
-                gameMatrix[playerIndexes[0]][playerIndexes[1] - 1] = tempPlayer
-            }
-            break;
-        default:
-            console.log("Invalid direction!")
-            break;
-    }
-
-    let zombiePositions = moveZombies()
-    let result = checkResult()
-}
-
-
-
-function startNewGame() {
-    // debugger
-    console.log('Starting new game')
-    wipeOldGameWindow()
-    fillMatrixItems()
-    createDivMatrix()
-}
-
-function wipeOldGameWindow() {
-    matrixItems.length = 0
-    catPositions.length = 0
-
-    let gameWindow = document.getElementById('game-window')
-
-    // Remove all child elements of gameWindow
-    while (gameWindow.firstChild) {
-        gameWindow.removeChild(gameWindow.firstChild);
     }
 }
 
@@ -158,12 +69,111 @@ function createDivMatrix() {
         for (let j = 0; j < 5; j++) {
             let itemDiv = document.createElement('div')
             itemDiv.className = 'matrix-item'
-            itemDiv.id = `item-[${i}][${j}]`
+            itemDiv.id = `item-${i}${j}`
             itemDiv.style.backgroundImage = `url(${matrixItems[i][j].imagePath})`
             gameWindow.appendChild(itemDiv)
         }
-
     }
+}
+
+function startNewGame() {
+    // debugger
+    console.log('Starting new game')
+    wipeOldGameWindow()
+    fillMatrixItems()
+    createDivMatrix()
+}
+
+function movePlayer(direction) {
+    
+    let playerIndexes = findPlayerIndexes()
+    let tempPlayer = document.getElementById(`item-${playerIndexes[0]}${playerIndexes[1]}`)
+    let oldPosition 
+    debugger
+    switch (direction) {
+        case 'N':
+            if (playerIndexes[0] > 0) {
+                debugger
+                oldPosition = document.getElementById(`item-${playerIndexes[0]}${playerIndexes[1]}`)
+                let background = new Forest()
+                oldPosition.style.backgroundImage = background.imagePath
+                matrixItems[playerIndexes[0]][playerIndexes[1]] = background           
+                newPosition = document.getElementById(`item-${playerIndexes[0] - 1}${playerIndexes[1]}`)
+                newPosition = tempPlayer
+                newPosition.style.backgroundImage = `url(${tempPlayer.imagePath})`
+            }
+            break;
+        case 'E':
+            if (playerIndexes[1] < matrixItems[0].length - 1) {
+                matrixItems[playerIndexes[0]][playerIndexes[1]] = new Forest()
+                matrixItems[playerIndexes[0]][playerIndexes[1] + 1] = tempPlayer
+            }
+            break;
+        case 'S':
+            if (playerIndexes[0] < matrixItems.length - 1) {
+                matrixItems[playerIndexes[0]][playerIndexes[1]] = new Forest()
+                matrixItems[playerIndexes[0] + 1][playerIndexes[1]] = tempPlayer
+            }
+            break;
+        case 'W':
+            if (playerIndexes[1] > 0) {
+                matrixItems[playerIndexes[0]][playerIndexes[1]] = new Forest()
+                matrixItems[playerIndexes[0]][playerIndexes[1] - 1] = tempPlayer
+            }
+            break;
+        default:
+            console.log("Invalid direction!")
+            break;
+    }
+
+    // let zombiePositions = moveZombies()
+    // let result = checkResult()
+}
+
+function findPlayerIndexes() {
+    let playerIndexes = []
+    let playerFound = false
+    // debugger
+    for (let i = 0; i < matrixItems.length; i++) {
+        for (let j = 0; j < matrixItems[i].length; j++) {
+            if (matrixItems[i][j] instanceof Player) {
+                playerIndexes.push(i)
+                playerIndexes.push(j)
+                playerFound = true
+                break
+            }
+        }
+        if (playerFound) break
+    }
+    // debugger
+    return playerIndexes
+}
+
+function wipeOldGameWindow() {
+    matrixItems.length = 0
+    catPositions.length = 0
+
+    let gameWindow = document.getElementById('game-window')
+    while (gameWindow.firstChild) {
+        gameWindow.removeChild(gameWindow.firstChild);
+    }
+}
+
+function checkResult(newPosition) {
+    for (let i = 0; i < zombiePositions.length - 1; i++)
+        if (newposition[0] == zombiePositions[i][0] && newposition[1] == zombiePositions[i][1]) {
+            return 'You died!!'
+        }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function moveZombies() {
+
 }
 
 
